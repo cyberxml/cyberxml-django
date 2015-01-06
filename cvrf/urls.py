f0498e2a4635aceedd5fc42238264ab42c899b5f
cyberxml <cyberxml@mail.com>
2015-01-06 00:10:11 +0000

    cvrf views

--- cvrf/urls.py
+++ cvrf/urls.py
@@ -5,5 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = patterns('cvrf.views',
 	(r'^hello', TemplateView.as_view(template_name='cvrf_hello.html')),
 	url(r'^rawxml/([mM][sS]\d{2}-\d{3})', views.rawxml),
+	url(r'^prettyxml/([mM][sS]\d{2}-\d{3})', views.prettyxmltest),
 	url(r'^pretty/([mM][sS]\d{2}-\d{3})', views.prettyxml),
 )
--- cvrf/views.py
+++ cvrf/views.py
@@ -19,18 +19,19 @@ def rawxml(request,msnum):
 		except:
 			raise Http404
 	else:
-		raise Http404
+		pass
+		#raise Http500
 
 # Create your views here.
-def prettyxml2(request,msnum):
+def prettyxmltest(request,msnum):
 	msnum = msnum.lower()
 	if not remsnum.match(msnum)==None:
 		try:
 			rootObj = cvrf.parse("static/data/microsoft.com/MSRC-CVRF/"+msnum+".xml",0)
 			xmlstr=StringIO()
 			rootObj.export(xmlstr, 0)
-			#return HttpResponse(xmlstr.getvalue(), content_type="application/xml")
-			return render(request, 'cvrf_pretty.html', {'test':msnum})
+			#return render(request, 'cvrf_pretty.html', {'test':msnum})
+			return render(request, 'cvrf_test.html', {'test':rootObj})
 		except:
 			raise Http404
 	else:
@@ -43,17 +44,25 @@ def prettyxml(request,msnum):
 	rootObj = cvrf.parse("static/data/microsoft.com/MSRC-CVRF/"+msnum+".xml",0)
 	cd = models.cvrfdoc_model()
 	
+	# ---------------------------------------------------------------------	
 	cd.DocumentTitle = models.DocumentTitleType_model(rootObj.DocumentTitle.get_valueOf_())
+	# ---------------------------------------------------------------------	
 	
+	# ---------------------------------------------------------------------	
 	cd.DocumentType = models.DocumentTypeType_model(rootObj.DocumentType.get_valueOf_())
+	# ---------------------------------------------------------------------	
 	
+	# ---------------------------------------------------------------------	
 	cd.DocumentPublisher = models.DocumentPublisherType_model()
+	# ---------------------------------------------------------------------	
 	cd.DocumentPublisher.VendorID = rootObj.DocumentPublisher.VendorID
 	cd.DocumentPublisher.Type = rootObj.DocumentPublisher.Type
 	cd.DocumentPublisher.ContactDetails = models.ContactDetailsType_model(rootObj.DocumentPublisher.ContactDetails.get_valueOf_())
 	cd.DocumentPublisher.IssuingAuthority = models.IssuingAuthorityType_model(rootObj.DocumentPublisher.IssuingAuthority.get_valueOf_())
 	
+	# ---------------------------------------------------------------------	
 	cd.DocumentTracking = models.DocumentTrackingType_model()
+	# ---------------------------------------------------------------------	
 	cd.DocumentTracking.Identification = models.IdentificationType_model()
 	cd.DocumentTracking.Identification.ID = models.IDType_model(rootObj.DocumentTracking.Identification.get_ID().get_valueOf_()) 
 	# there is a list of Aliasess for Identification
@@ -76,7 +85,9 @@ def prettyxml(request,msnum):
 	# Engine
 	# Date
 	
+	# ---------------------------------------------------------------------	
 	cd.DocumentNotes = models.DocumentNotesType_model()
+	# ---------------------------------------------------------------------	
 	# there is a list of Notes for DocumentNotes
         cd.DocumentNotes.Note = models.NoteType_model(rootObj.DocumentNotes.get_Note()[0].get_valueOf_()) 
 	cd.DocumentNotes.Note.Ordinal = rootObj.DocumentNotes.get_Note()[0].get_Ordinal() 
@@ -84,8 +95,48 @@ def prettyxml(request,msnum):
 	cd.DocumentNotes.Note.Type = rootObj.DocumentNotes.get_Note()[0].get_Type() 
 	cd.DocumentNotes.Note.Title = rootObj.DocumentNotes.get_Note()[0].get_Title() 
 	
-	cd.AggregateSeverity = models.AggregateSeverityType_model()
+	# ---------------------------------------------------------------------	
 	cd.AggregateSeverity = models.AggregateSeverityType_model(rootObj.AggregateSeverity.get_valueOf_())
+	# ---------------------------------------------------------------------	
+	
+	# ---------------------------------------------------------------------	
+	cd.DocumentReferences = models.DocumentReferencesType_model()
+	# ---------------------------------------------------------------------	
+	# there is a list of References for DocumentReferences
+	cd.DocumentReferences.Reference = models.ReferenceType_model()
+        cd.DocumentReferences.Reference.URL = rootObj.DocumentReferences.get_Reference()[0].get_URL() 
+        cd.DocumentReferences.Reference.Type = rootObj.DocumentReferences.get_Reference()[0].get_Type() 
+	cd.DocumentReferences.Reference.Description  = models.DescriptionType1_model(rootObj.DocumentReferences.get_Reference()[0].get_Description().get_valueOf_())
+	
+	# ---------------------------------------------------------------------	
+	#cd.Acknowledgments = models.AcknowledgmentsType_model()
+	# ---------------------------------------------------------------------	
+	# there is a list of Acknowledgements  for Acknowledgements
+	#cd.Acknowledgments.Acknowledgement = models.AcknowledgmentType_model()
+	#cd.Acknowledgments.Acknowledgement.URL = rootObj.Acknowledgements.get_Acknowledgement()[0].get_URL() 
+	#cd.Acknowledgments.Acknowledgement = models.AcknowledgmentType_model()
+	
+	# ---------------------------------------------------------------------	
+	cd.ProductTree = models.ProductTree_model()
+	# ---------------------------------------------------------------------	
+	# If tree is flat, # branches = 0
+	# Branches can be nested to a depth of ?
+	#cd.ProductTree.Branch = models.BranchType_model()
+	#cd.ProductTree.Branch.Type = rootObj.ProductTree.get_Branch()[0].get_Type() 
+	#cd.ProductTree.Branch.Name = rootObj.ProductTree.get_Branch()[0].get_Name() 
+	# there is a list of FullProductNames  for ProductTree
+	pt = rootObj.ProductTree.get_FullProductName()
+	fpn=[]
+	for i in range(len(pt)):
+		this = models.FullProductName_model(rootObj.ProductTree.get_FullProductName()[i].get_valueOf_())
+		this.CPE = rootObj.ProductTree.get_FullProductName()[i].get_CPE()
+		this.ProductID = rootObj.ProductTree.get_FullProductName()[i].get_ProductID()
+		fpn.append(this)
+	cd.ProductTree = models.ProductTree_model(fpn)
+	cd.ProductTree.Relationship = models.RelationshipType_model()
+	# more
+	cd.ProductTree.ProductGroups = models.ProductGroupsType_model()
+	# more
 	
 	return render(request, 'cvrf_table.html', {'cvrfdoc':cd})
 	
