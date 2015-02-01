08934df88eb9bce8e87ce37b41a983c99d608987
cyberxml <cyberxml@mail.com>
2015-02-01 18:40:42 +0000

    static refactor

--- cyberxml/settings.py
+++ cyberxml/settings.py
@@ -89,9 +89,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = (
-	os.path.join(BASE_DIR, "static/"),
+	os.path.join(BASE_DIR, "cyberxml/static/"),
 )
 
 # include the eulexistdb settings 
