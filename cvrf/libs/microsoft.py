2674a939c7a6edda89bf7d63477d7c3106fc6122
rbroberg <ronald.broberg@mdnt.com>
2015-03-20 16:46:17 -0600

    soft match for CVE; whitespace in CVRF

--- cvrf/libs/microsoft.py
+++ cvrf/libs/microsoft.py
@@ -22,7 +22,7 @@ def get_qryMicrosoftCvrfCveCpe(cve):
 	declare namespace prod = "http://www.icasi.org/CVRF/schema/prod/1.1";
 	declare namespace vuln = "http://www.icasi.org/CVRF/schema/vuln/1.1";
 	let $thiscve := "'''+cve+'''"
-	for $pid in collection('/db/cyberxml/data/cvrf/microsoft.com')//node()[vuln:CVE[.=$thiscve]]/vuln:ProductStatuses/vuln:Status[@Type="Known Affected"]/vuln:ProductID/text()
+	for $pid in collection('/db/cyberxml/data/cvrf/microsoft.com')//node()[vuln:CVE[contains(.,$thiscve)]]/vuln:ProductStatuses/vuln:Status[@Type="Known Affected"]/vuln:ProductID/text()
 	for $cpe in doc('/db/cyberxml/apps/cvrf/microsoft.com/msprodid2cpe.xml')//node()[ProductID=$pid]/CPE2.2/text()
 	return $cpe'''
 	return qry
